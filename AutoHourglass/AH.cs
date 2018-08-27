0b56b2a6b0adaac1641897d60f7afcd209f7a1ed
KU <ku@digitaldolphins.jp>
2018-08-27 13:09:43 +0900

    0.3

---
+++ AutoHourglass/AH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kenjiuno.AutoHourglass
+{
+    public class AH : IDisposable
+    {
+        Cursor previous;
+
+        public AH()
+        {
+            previous = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+        }
+
+        public void Dispose()
+        {
+            Cursor.Current = previous ?? Cursor.Current;
+            previous = null;
+        }
+    }
+}
